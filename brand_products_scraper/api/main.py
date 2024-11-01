bd222957552110fb59ea68a934704f1fe92ff72f
harikeerthan24 <harihemu2001@gmail.com>
2024-11-01 19:20:17 +0530

    Altered the main.py

--- brand_products_scraper/api/main.py
+++ brand_products_scraper/api/main.py
@@ -10,6 +10,7 @@ from endpoints.burberry_scraper_api import router as burberry_router
 # from endpoints.zara_scraper_api import router as zara_router
 from endpoints.kate_spade_api import router as kate_spade_router
 from endpoints.uniqlo_scraper_api import router as uniqlo_router
+from endpoints.nike_scraper_api import router as nike_router
 
 app = FastAPI(
     title="Brand Products Scraper API",
@@ -22,6 +23,8 @@ app.include_router(burberry_router,tags=["Burberry"]) # Burberry
 # app.include_router(zara_router, tags=["Zara"])  # Zara 
 app.include_router(kate_spade_router , tags=["Kate Spade"]) # Kate_Spade
 app.include_router(uniqlo_router, tags=["Uniqlo"]) # Uniqlo
+app.include_router(nike_router, tags=["Nike"]) # Nike
+
 
 
 if __name__ == "__main__":
